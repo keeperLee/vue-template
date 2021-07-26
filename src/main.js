970ac5b29faedc92e28eea667f71d17d2ab1dc8d
lijian <1376623234@qq.com>
2021-07-26 16:09:31 +0800

    安装axios,编写请求后端的案例

--- src/main.js
+++ src/main.js
@@ -4,7 +4,11 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import Axios from 'axios'
+
+
 Vue.use(ElementUI)
+Vue.prototype.$axios = Axios
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/About.vue
+++ src/views/About.vue
@@ -4,3 +4,45 @@
         <el-button>element-ui的按钮</el-button>
     </div>
 </template>
+
+
+<script>
+export default {
+    name:"about",
+    data(){
+        return {
+
+        }
+    },
+    mounted() {
+        this.queryAllUser();
+        this.queryUserById();
+    },
+    methods: {
+        queryAllUser() {
+            this.$axios.get("http://localhost:8083/user/getAllUser").then(function (response) {
+                // handle success
+                console.log(response);
+            }).catch(function (error) {
+                // handle error
+                console.log(error);
+            }).then(function () {
+                // always executed
+            });
+        },
+        queryUserById() {
+            this.$axios.get("http://localhost:8083/user/getUserById",{params:{
+                    id:'3'
+                }}).then(function (response) {
+                // handle success
+                console.log(response);
+            }).catch(function (error) {
+                // handle error
+                console.log(error);
+            }).then(function () {
+                // always executed
+            });
+        }
+    }
+}
+</script>
