0899737ffa3344e06e79ba4d09ad85f8d8e68c10
lijian <1376623234@qq.com>
2021-07-26 18:25:21 +0800

    简单添加列表

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>|
-      <router-link to="/echarts">Echarts</router-link>
+      <router-link to="/echarts">Echarts</router-link>|
+      <router-link to="/element">Element</router-link>
     </div>
     <router-view/>
   </div>
@@ -11,10 +12,17 @@
 
 <style lang="stylus">
 #app
+  position absolute
+  width 100%
+  height 100%
   font-family Avenir, Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
   text-align center
   color #2c3e50
   margin-top 60px
+    #nav
+        position relative
+        width 80%
+        height 80%
 </style>
