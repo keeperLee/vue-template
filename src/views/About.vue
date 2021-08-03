c185f625271946c6711526257d3ada841542327c
lijian <1376623234@qq.com>
2021-08-03 11:23:23 +0800

    elementui的图标

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="about">
         <h1>This is an about page</h1>
+        <i class="el-icon-share" style="color: red"></i>
         <el-button>element-ui的按钮</el-button>
+        <el-button type="primary" icon="el-icon-search">搜索</el-button>
+        <el-button type="primary" icon="el-icon-platform-eleme">饿了么</el-button>
     </div>
 </template>
 
