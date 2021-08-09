e2951722de4b14f7485232ba7003886518cb766c
lijian <1376623234@qq.com>
2021-08-09 09:49:13 +0800

    map使用

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,24 +5,53 @@
         <el-button>element-ui的按钮</el-button>
         <el-button type="primary" icon="el-icon-search">搜索</el-button>
         <el-button type="primary" icon="el-icon-platform-eleme">饿了么</el-button>
-        {{getStateNumber}}
+        {{ getStateNumber }}
+        <el-backtop target=".page-component__scroll .el-scrollbar__wrap" :bottom="100">
+            <div class="back-to-up">
+                UP
+            </div>
+        </el-backtop>
+        <p>段落</p>
     </div>
 </template>
 
 
 <script>
-export default {
-    name:"about",
-    data(){
-        return {
+import * as d3 from 'd3'
 
-        }
+export default {
+    name: "about",
+    data() {
+        return {}
     },
     mounted() {
         this.queryAllUser();
         this.queryUserById();
+        this.draw();
+        this.testMap();
     },
     methods: {
+        testMap() {
+            let map = new Map([
+                [1, 'one'],
+                [2, 'two'],
+                [3, 'three'],
+            ]);
+            console.log(typeof map)
+            for(let key of map.keys()){
+                console.log(key)
+            }
+            for(let value of map.values()){
+                console.log(value)
+            }
+            for(let item of map.entries()){
+                console.log(item)
+            }
+            // let cloneMap = JSON.parse(JSON.stringify(map));
+            // for(let key of cloneMap.keys()){
+            //     console.log(key)
+            // }
+        },
         queryAllUser() {
             this.$axios.get("http://localhost:8083/user/getAllUser").then(function (response) {
                 // handle success
@@ -35,9 +64,11 @@ export default {
             });
         },
         queryUserById() {
-            this.$axios.get("http://localhost:8083/user/getUserById",{params:{
-                    id:'3'
-                }}).then(function (response) {
+            this.$axios.get("http://localhost:8083/user/getUserById", {
+                params: {
+                    id: '3'
+                }
+            }).then(function (response) {
                 // handle success
                 console.log(response);
             }).catch(function (error) {
@@ -46,12 +77,21 @@ export default {
             }).then(function () {
                 // always executed
             });
+        },
+        draw() {
+            d3.selectAll("p").style("color", function () {
+                return "hsl(" + Math.random() * 360 + ",100%,50%)";
+            });
         }
     },
-    computed:{
-        getStateNumber(){
+    computed: {
+        getStateNumber() {
             return this.$store.getters.getNumberOne;
         }
     }
 }
 </script>
+
+<style scoped lang="">
+
+</style>
