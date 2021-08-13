a8dbb85ab1cebbfca4f002db7930759ece395683
lijian <1376623234@qq.com>
2021-08-13 09:33:53 +0800

    maptalks-监听地图事件

--- src/components/map/MapTalksDemo.vue
+++ src/components/map/MapTalksDemo.vue
@@ -1,7 +1,11 @@
 <template>
-    <div id="map" class="container">
+    <div class="root">
+        <div id="map" class="container">
 
+        </div>
+        <div id="info"></div>
     </div>
+
 </template>
 
 <script>
@@ -11,7 +15,9 @@ export default {
     name: 'MapTalksDemo',
     mounted() {
         // this.drawBaseMap();
-        this.drawBaseMapArea();
+        // this.drawBaseMapArea();
+        // this.preventZoom();
+        this.mapEventListener();
     },
     methods: {
         //创建基层地图
@@ -30,7 +36,7 @@ export default {
         drawBaseMapArea() {
             const map = new maptalks.Map('map', {
                 center: [113.27, 23.13],
-                centerCross : true,//展示屏幕中心的准心
+                centerCross: true,//展示屏幕中心的准心
                 zoom: 16, //放大倍数
                 baseLayer: new maptalks.TileLayer('base', {
                     urlTemplate: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
@@ -42,21 +48,56 @@ export default {
                 ]
             });
 
-            var extent = map.getExtent();
+            const extent = map.getExtent();
             // console.log(extent,'extent');
             // console.log(extent.toArray(),'extent.toArray()');
-
             // set map's max extent to map's extent at zoom 14
             map.setMaxExtent(extent);
-
             map.setZoom(map.getZoom() - 2, {animation: true});
-
             map.getLayer('v')
                     .addGeometry(
                             new maptalks.Polygon(extent.toArray(), {
                                 symbol: {'polygonOpacity': 1, 'lineWidth': 2}
                             })
                     );
+        },
+        //禁止缩放
+        preventZoom() {
+            const map = new maptalks.Map('map', {
+                center: [-0.113049, 51.498568],
+                zoom: 14,
+                minZoom: 14, // set map's min zoom to 14
+                maxZoom: 14, // set map's max zoom to 14
+                baseLayer: new maptalks.TileLayer('base', {
+                    urlTemplate: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
+                    subdomains: ['a', 'b', 'c', 'd'],
+                    attribution: '&copy; <a href="http://osm.org">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/">CARTO</a>'
+                })
+            });
+
+            // or you can set zoom limit by
+            map.setMinZoom(14).setMaxZoom(14);
+        },
+        //地图事件监听
+        mapEventListener() {
+            const map = new maptalks.Map('map', {
+                center: [113.27, 23.13],
+                zoom: 14,
+                baseLayer: new maptalks.TileLayer('base', {
+                    urlTemplate: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
+                    subdomains: ['a', 'b', 'c', 'd'],
+                    attribution: '&copy; <a href="http://osm.org">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/">CARTO</a>'
+                })
+            });
+
+            map.on('click', function (param) {
+                const infoDom = document.getElementById('info');
+                console.log(infoDom)
+                console.log(param);
+                infoDom.innerHTML = '<div>' + new Date().toLocaleTimeString() +
+                        ': click map on ' + param.coordinate.toFixed(5).toArray().join() + '</div>' +
+                        infoDom.innerHTML;
+            });
         }
     }
 }
@@ -64,7 +105,24 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus">
-.container
+.root
     width 100%
     height 100%
+
+    .container
+        width 100%
+        height 100%
+
+    #info {
+        position: fixed;
+        background-color: rgba(13, 13, 13, 0.5);
+        padding: 10px 10px 10px 10px;
+        font: 13px bold sans-serif;
+        color: #fff;
+        left: 0px;
+        top: 0px;
+        width: 100%;
+        height: 140px;
+        overflow: hidden
+    }
 </style>
